0e84fe58d6d846ba64ed126893f3d5870a24d264
zigzag <zigzag@localhost.localdomain>
2018-11-22 10:49:58 +0100

    ajout json

--- appli/server.js
+++ appli/server.js
@@ -4,6 +4,7 @@ var app            = express();
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var socketio	   = require('socket.io')
 
 // configuration ===========================================
 	
@@ -11,7 +12,7 @@ var methodOverride = require('method-override');
 var db = require('./config/db');
 
 var port = process.env.PORT || 8080; // set our port
-// mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+//mongoose.connect('mongodb://localhost/database'); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
@@ -20,6 +21,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-f
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+app.get('/',function(req,res){
+    res.sendFile(path.normalize(__dirname + 'bson/data.json'))
+})
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
